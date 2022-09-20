93d7cd19913648fe3da3ddf4254179c4ee9a3602
riyanmgeorge <110010573+riyanmgeorge@users.noreply.github.com>
2022-09-20 12:15:52 +0530

    Fourth Commit

--- Main.py
+++ Main.py
@@ -1,11 +1,20 @@
 import re
+
 def display_hash(hashtable) -> None:
-	
+ 
+for keyvalue in range(len(hashtable)):
+  content = hashtable[keyvalue]
+  print(keyvalue, end = " ")
+  
+for element in content:
+   print("-->", element, end = " ")
+  print()
+
 def Hashing(keyvalue) -> int:
-	return keyvalue % len(HashTable)
+ return keyvalue % len(HashTable)
 
 def insert(Hashtable, keyvalue, value) -> None:
-	
+ Hashtable[Hashing(keyvalue)].append(value)
 
 
 # Do not edit the following code
@@ -20,4 +29,4 @@ for item in re.split('], |].', input_data):
   if len(data) > 1:
     insert(HashTable, int(data[0]), data[1])
 
-display_hash (HashTable)
+display_hash(HashTable)
